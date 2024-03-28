9793fa44e6acad3a75f65e8a2344b38df0ae4b7f
Charles Henville <henvillecharles@gmail.com>
2024-03-27 20:55:11 -0400

    Finalized coordinate transformations

--- fluid_simulator.c
+++ fluid_simulator.c
@@ -185,7 +185,7 @@ void drawBresenhamLine(int x0, int y0, int x1, int y1, short int colour){
 //                                          FLUID SIMULATION UTILS
 // =======================================================================================================
 
-#define NUM_PARTICLES       48
+#define NUM_PARTICLES       12
 
 #define WATER_COLOUR        27743
 #define WATER_HUE           0.62
@@ -193,19 +193,20 @@ void drawBresenhamLine(int x0, int y0, int x1, int y1, short int colour){
 #define PRESSURE_COLUMNS    24
 #define PRESSURE_ROWS       32
 
-#define G                   10
-#define K                   1
+#define G                   9.81
+#define K                   50.0
 #define PARTICLE_MASS       1
 #define SPF                 0.01 // Seconds Per Frame
-#define ELASTICITY          0.6 // 0 to 1
+#define ELASTICITY          0.4 // 0 to 1
 #define VELOCITY_COLOUR_SENSITIVITY 200.0
 #define VISCOSITY           0.2
-#define ROOT_TWO_SCALE      1.5
+#define ROOT_TWO_SCALE      1.42
 
-#define M_PER_PX            0.001
-#define PX_PER_M            1000.0
+#define M_PER_PX            0.01
+#define PX_PER_M            100.0
 
-#define DENSITY_RESTING     100
+#define DENSITY_RESTING     1000.0
+	
 float h; // Spacing parameter between fluids in the simulation
 float inv_rho_naught;
 float alpha; // Cubic Bezier Constant for W_ij calc
@@ -219,10 +220,14 @@ typedef struct drawParticle {
 typedef struct Particle {
 
     int x, y;
+    float pX, pY;
     float vx, vy;
     float ax, ay;
     float pressure, density;
     bool neighbours[NUM_PARTICLES];
+    float neighbourDXs[NUM_PARTICLES];
+    float neighbourDYs[NUM_PARTICLES];
+    float neighbourDistances[NUM_PARTICLES];
     short int colour;
 
 } Particle;
@@ -245,16 +250,17 @@ void initParticles() {
     alpha = 5.0/(14.0*M_PI*h*h);
     inv_rho_naught = 1.0/(float)DENSITY_RESTING;
     
-    // DEBUGE
-    printf("\nh: %d", h);
+    // DEBUG
+    printf("\nh: %f", h);
     printf("\nalpha: %f", alpha);
     printf("\ninv density: %f", inv_rho_naught);
 
     int xStepCount = 0;
     int yStepCount = 0;
-
+	
     for (int i = 0; i < NUM_PARTICLES; i++) {
-
+		
+		srand(i);
         if(xStepCount >= amtColumns) {
             xStepCount = 0;
             yStepCount++;
@@ -262,14 +268,16 @@ void initParticles() {
         if(yStepCount >= amtRows) {
             yStepCount = 0;
         }
-        allParticles[i].x = initX + xStepCount*stepX;
-        allParticles[i].y = initY + yStepCount*stepY;
+        allParticles[i].x = initX + xStepCount*stepX + (rand() % 3) - 1;
+        allParticles[i].y = initY + yStepCount*stepY + (rand() % 3) - 1;
         allParticles[i].vx = 0;
         allParticles[i].vy = 0;
         allParticles[i].colour = WATER_COLOUR;
 
         xStepCount++;
 
+        allParticles[i].pX = M_PER_PX * allParticles[i].x;
+        allParticles[i].pY = M_PER_PX * allParticles[i].y;
         allEraseParticles[i].x = allParticles[i].x;
         allEraseParticles[i].y = allParticles[i].y;
     }
@@ -294,33 +302,35 @@ void stepSPHPositions(int i) {
     allEraseParticles[i].x = allParticles[i].x;
     allEraseParticles[i].y = allParticles[i].y;
 
-    allParticles[i].x += PX_PER_M*allParticles[i].vx*SPF;
-    allParticles[i].y += PX_PER_M*allParticles[i].vy*SPF;
+    allParticles[i].pX += allParticles[i].vx * SPF;
+    allParticles[i].pY += allParticles[i].vy * SPF;
+    allParticles[i].x = PX_PER_M * allParticles[i].pX;
+    allParticles[i].y = PX_PER_M * allParticles[i].pY;
 
     // If, for whatever reason, we went out of bounds after velocity application, fix them manually.
-    if (allParticles[i].x < 0 || allParticles[i].x > (MAX_X-1)) {
-        if (allParticles[i].x < 0){
-            allParticles[i].x = 0;
-        } else {
-            allParticles[i].x = MAX_X-1;
-        }
-    }
-    if (allParticles[i].y < 0 || allParticles[i].y > (MAX_Y-1)) {
-        if (allParticles[i].y < 0){
-            allParticles[i].y = 0;
-        } else {
-            allParticles[i].y = MAX_Y-1;
-        }
-    }
-
+	if (allParticles[i].x <= 0){
+		allParticles[i].x = 0;
+	} else if (allParticles[i].x > (MAX_X - 1)) {
+		allParticles[i].x = MAX_X-1;
+	}
+	if (allParticles[i].y < 0){
+		allParticles[i].y = 0;
+	} else if (allParticles[i].y > (MAX_Y -1)){
+		allParticles[i].y = MAX_Y-1;
+	}
+    
 }
 
 void stepSPHVelocities(int i) {
     // Border collision handling, maybe add h deltas to these checks?
-    if(allParticles[i].x <= 0 || allParticles[i].x >= (MAX_X-1)) {
+    if((allParticles[i].x >= (MAX_X-1) && allParticles[i].vx > 0) || (allParticles[i].x <= 0 && allParticles[i].vx < 0)) {
+        if(allParticles[i].x <= 0){
+            printf("\nBSG::%f", allParticles[i].vx);
+            printf("\nBSGGG::%f", -allParticles[i].vx*ELASTICITY);
+        }
         allParticles[i].vx = -allParticles[i].vx*ELASTICITY;
     }
-    if(allParticles[i].y <= 0 || allParticles[i].y >= (MAX_Y-1)) {
+    if((allParticles[i].y >= (MAX_Y-1) && allParticles[i].vy > 0) || (allParticles[i].y <= 0 && allParticles[i].vy < 0)) {
         allParticles[i].vy = -allParticles[i].vy*ELASTICITY;
     }
     allParticles[i].vx += allParticles[i].ax*SPF;
@@ -329,7 +339,9 @@ void stepSPHVelocities(int i) {
 
 void calculateSPHAccelerations(int i) {
 
-    float ax=0, ay=0;
+	allParticles[i].ax = 0;
+    allParticles[i].ay = G; // Gravitational Acceleration
+	
     float GRADW_ijx, GRADW_ijy; // Derivatives of the same Kernel we saw in the function that invokes this one
     float dx, dy;
     float dvx, dvy;
@@ -339,15 +351,16 @@ void calculateSPHAccelerations(int i) {
     float pressureRatio_i = allParticles[i].pressure / (allParticles[i].density * allParticles[i].density);
     float inv_rho_j, pressureRatio_j;
 
-    for (int j = i + 1; j < NUM_PARTICLES; j++) {
+    for (int j = 0; j < NUM_PARTICLES; j++) {
 
-        if(!allParticles[i].neighbours[j]) continue; // Dont check non-neighbours
+        if (!allParticles[i].neighbours[j]) continue; // Dont check non-neighbours
+        if (allParticles[i].neighbourDistances[j] == 0) continue; // Everything goes to zero if no distance
 
-        dx = M_PER_PX * (allParticles[i].x - allParticles[j].x);
-        dy = M_PER_PX * (allParticles[i].y - allParticles[j].y);
-        x_ij2 = dx*dx+dy*dy;
-        if (!x_ij2) continue;
-        x_ij = sqrt(x_ij2);
+        dx = allParticles[i].neighbourDXs[j];
+        dy = allParticles[i].neighbourDYs[j];
+
+        x_ij = allParticles[i].neighbourDistances[j];
+        x_ij2 = x_ij*x_ij;
 
         q = x_ij/h;
         if(q < 1){
@@ -365,23 +378,19 @@ void calculateSPHAccelerations(int i) {
 
         inv_rho_j = 1/allParticles[j].density;
         pressureRatio_j = allParticles[j].pressure * inv_rho_j * inv_rho_j;
-        ax -= (pressureRatio_i + pressureRatio_j) * GRADW_ijx;
-        ay -= (pressureRatio_i + pressureRatio_j) * GRADW_ijy;
+        allParticles[i].ax -= (pressureRatio_i + pressureRatio_j) * GRADW_ijx;
+        allParticles[i].ay -= (pressureRatio_i + pressureRatio_j) * GRADW_ijy;
 
         // Viscosity Acceleration
 
         dvx = allParticles[i].vx - allParticles[j].vx;
         dvy = allParticles[i].vy - allParticles[j].vy;
 
-        viscosScale = 2 * VISCOSITY * inv_rho_j * (dx*GRADW_ijx + dy*GRADW_ijy) / (x_ij2+h*h/100);
-        ax += viscosScale * dvx;
-        ay += viscosScale * dvy;
+        viscosScale = (VISCOSITY+VISCOSITY) * inv_rho_j * (dx*GRADW_ijx + dy*GRADW_ijy) / (x_ij2+h*h/100.0);
+        allParticles[i].ax += viscosScale * dvx;
+        allParticles[i].ay += viscosScale * dvy;
 
     }
-    ay+=G; // Gravitational Acceleration
-
-    allParticles[i].ax = ax;
-    allParticles[i].ay = ay;
 
 }
 
@@ -394,11 +403,23 @@ void timeStepSPHApproximation() {
 
         float dx, dy;
         float x_ij, q, rho = 0;
+		
         for (int j = i + 1; j < NUM_PARTICLES; j++) {
-            dx = M_PER_PX * (allParticles[i].x - allParticles[j].x);
-            dy = M_PER_PX * (allParticles[i].y - allParticles[j].y);
+			
+            dx = allParticles[i].pX - allParticles[j].pX;
+            dy = allParticles[i].pY - allParticles[j].pY;
             x_ij = sqrt(dx*dx+dy*dy);
+			
             if (x_ij<ROOT_TWO_SCALE*h) {
+
+                allParticles[i].neighbourDXs[j] = dx;
+                allParticles[i].neighbourDYs[j] = dy;
+                allParticles[i].neighbourDistances[j] = x_ij;
+
+                allParticles[j].neighbourDXs[i] = -dx;
+                allParticles[j].neighbourDYs[i] = -dy;
+                allParticles[j].neighbourDistances[i] = x_ij;
+
                 allParticles[i].neighbours[j] = true;
                 allParticles[j].neighbours[i] = true;
 
@@ -421,11 +442,12 @@ void timeStepSPHApproximation() {
                 allParticles[i].neighbours[j] = false;
                 allParticles[j].neighbours[i] = false;
             }
+			
         }
 
         allParticles[i].pressure = K * pow((allParticles[i].density*inv_rho_naught), 7) - K;
 
-        if(i==0||i==NUM_PARTICLES-1){
+        // if(i==0||i==NUM_PARTICLES-1){
             // printf("\n\n***PARTICLE %d***", i);
             // printf("\ndensity: %f", allParticles[i].density);
             // printf("\npressure: %f", allParticles[i].pressure);
@@ -435,7 +457,7 @@ void timeStepSPHApproximation() {
             // printf("\nvy: %f", allParticles[i].vy);
             // printf("\nax: %f", allParticles[i].ax);
             // printf("\nay: %f", allParticles[i].ay);
-        }
+        // }
 
         // 3. Calculate Accelearations (Approx)
         calculateSPHAccelerations(i);
